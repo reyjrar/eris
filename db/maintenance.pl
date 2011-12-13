4169d1ed2c7ba1c6a8b843c77d8ada7ff99b6042
Brad Lhotsky <brad.lhotsky@gmail.com>
2011-12-13 15:04:51 -0500

    removed tables that no longer exist

--- db/maintenance.pl
+++ db/maintenance.pl
@@ -15,15 +15,13 @@ my $ETC = "$FindBin::Bin/../etc";
 my $CFG = YAML::LoadFile( qq{$ETC/eris.cfg} );
 
 my @tables = qw(
-	authen_current certification_personal_devices
+	authen_current
 	device_class_mapping device_classes device_details
 	device_discovery device_waivers
 	eris_node_types eris_ous eris_role_map eris_roles
 	eris_users ext_custodians ext_lab_pco ext_property_book
 	ext_surplus_sheets fake_mac_addr inventory_archive	
 	notification_admins notification_meta notification_queue
-	portable_cert_extensions portable_cert_fol_sensitive_data
-	portable_cert_laptops portable_cert_personal portable_cert_responses
 	regulatory_application regulatory_compliance regulatory_device_log
 	regulatory_exception_classes regulatory_meta regulatory_waiver_log
 	regulatory_waivers regulatory_waivers_meta
